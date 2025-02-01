61878a4031662897b4b8987325bd7faffa955233
MassimilianoRocco <massimilianorocco93@gmail.com>
2025-02-01 10:15:22 +0100

    card component + general fix

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Movie } from './Movie.model';
 
 @Component({
   selector: 'app-card',
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
+  host:{
+    
+  }
 })
 export class CardComponent {
 
+  movie = input<Movie>()
+
+  
 }
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,16 +1,17 @@
 import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { MoviesService } from './movies.service';
+import { CardComponent } from "../card/card.component";
 
 @Component({
   selector: 'app-movies',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.css'
 })
 export class MoviesComponent implements OnInit {
 
   movies = signal<any[]>([]);
-  
+
 
   private moviesService = inject(MoviesService);
 
@@ -21,14 +22,14 @@ export class MoviesComponent implements OnInit {
       next: (data) => {
         console.log(data)
         this.movies.set(data)
-        },
-        error: (error) => {
-          console.error('Error:', error);
-          }
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      }
     });
 
   }
 
- 
+
 
 }
